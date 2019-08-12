40911bc9a27d006d9f0a4a4dbcd02c6b3429fd7a
Sachin <singh.s.kshetriya@gmail.com>
2019-08-12 19:11:41 +0530

    Crawling

---
+++ web.py
@@ -0,0 +1,2 @@
+import requests
+page = "https://movement.uber.com/explore/bangalore/travel-times/query?lang=hi-IN&si=89&ti=94&ag=wards&sa;=&ta;=&dt[tpb]=ALL_DAY&dt[wd;]=1,2,3,4,5,6,7&dt[dr][sd]=2018-12-01&dt[dr][ed]=2018-12-31&cd=&lat.=12.9410403&lng.=77.585093&z.=10.79&sdn=&tdn="
